a4260ed2ba1bf1a5227763a543b20f37ee54610b
RahulChugwani <rahulchugwani12@gmail.com>
2021-05-05 15:20:24 +0530

    Intial Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cron = require('node-cron');
+const moment = require('moment');
+
+require('dotenv').config();
+
+const availableNotifier = require('./available-notifier');
+
+const app = express();
+const port = process.env.PORT || 4000 ;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once( 'open', ()=>{
+    console.log("Mongodb connection successful");
+});
+
+const usersRouter = require('./routes/user');
+app.use('/users', usersRouter);
+
+app.get('/', function (req, res) {
+  res.send('Hello World this is Vaccine Notifier!');
+});
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
+});
+
+availableNotifier.main();
